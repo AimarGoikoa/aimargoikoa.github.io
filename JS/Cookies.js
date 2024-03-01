0419ff5a4c25c607c304c2901958d1f13c337f2b
AimarGoikoa <agoikoasan1@educacion.navarra.es>
2024-03-01 09:29:42 +0100

    Aviso cookies

---
+++ JS/Cookies.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aceptarcookiesboton = document.getElementById("aceptarcookies");
+    const avisocookies = document.getElementById("avisocookies");
+
+    aceptarcookiesboton.addEventListener("click", function() {
+        avisocookies.style.display = "none";
+
+    });
+});
